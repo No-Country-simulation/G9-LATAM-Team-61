da1ef8ede29f8476614dbe293273f96c4536aa88
unknown <Francoise>
2026-07-28 00:26:26 -0500

    feat: aisla el mock controller con el perfil local

--- backend/src/main/java/com/g9_latam_team_61/backend/controller/FastApiMockController.java
+++ backend/src/main/java/com/g9_latam_team_61/backend/controller/FastApiMockController.java
@@ -2,11 +2,13 @@ package com.g9_latam_team_61.backend.controller;
 
 import com.g9_latam_team_61.backend.client.FastApiRequest;
 import com.g9_latam_team_61.backend.client.FastApiResponse;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Profile({"local", "mock", "test"})
 @RestController
 @RequestMapping("/analizar")
 public class FastApiMockController {
